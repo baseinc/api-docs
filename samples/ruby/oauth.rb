a2b46a8f33e0b33b55fdc547a3c0c912cb9519d3
Yosuke Kabuto <bluewhale1982@gmail.com>
2015-12-16 11:22:01 +0900

    Format code

--- samples/ruby/oauth.rb
+++ samples/ruby/oauth.rb
@@ -1,30 +1,31 @@
+# get access token from authentication code
+
 require 'net/http'
 require 'json'
 
-API_VERSION = 1
-API_HOST = 'api.thebase.in'
-CODE = ''
-CLIENT_ID = ''
+API_VERSION   = 1
+API_HOST      = 'api.thebase.in'
+CODE          = ''
+CLIENT_ID     = ''
 CLIENT_SECRET = ''
-REDIRECT_URI = ''
+REDIRECT_URI  = ''
 
-# get access token from authentication code
-request_parameters = {
-  grant_type: '',
-  client_id: CLIENT_ID,
-  client_secret: CLIENT_SECRET,
-  code: CODE,
-  redirect_uri: REDIRECT_URI
-}
+request_parameters =
+  { grant_type: '',
+    client_id: CLIENT_ID,
+    client_secret: CLIENT_SECRET,
+    code: CODE,
+    redirect_uri: REDIRECT_URI }
+
 uri =
   URI(
     API_HOST,
     "/#{API_VERSION}/oauth/token",
     request_parameters)
 
-file = File.stat 'cached_response'
-request = Net::HTTP::Post.new uri
-request.content_type = 'multipart/form-data'
+file                         = File.stat 'cached_response'
+request                      = Net::HTTP::Post.new uri
+request.content_type         = 'multipart/form-data'
 request['If-Modified-Since'] = file.mtime.rfc2822
 
 response =
